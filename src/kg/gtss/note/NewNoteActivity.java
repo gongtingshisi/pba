98194fe151fd6a91322466db33cc966a37532a25
fengzhang <fengzhang@lewatek.com>
2016-01-20 13:56:40 +0800

    Note related

--- src/kg/gtss/note/NewNoteActivity.java
+++ src/kg/gtss/note/NewNoteActivity.java
@@ -1,8 +1,21 @@
 package kg.gtss.note;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.graphics.PathEffect;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.widget.EditText;
 import kg.gtss.personalbooksassitant.R;
+import kg.gtss.utils.Log;
 
 public class NewNoteActivity extends Activity {
 
@@ -19,4 +32,60 @@ public class NewNoteActivity extends Activity {
 		super.onResume();
 	}
 
+	/**
+	 * A custom EditText that draws lines between each line of text that is
+	 * displayed.
+	 */
+	public static class LinedEditText extends EditText {
+		private Rect mRect;
+		private Paint mPaint;
+		private static final String TAG = "NoteEditText";
+		private static final String SCHEME_TEL = "tel:";
+		private static final String SCHEME_HTTP = "http:";
+		private static final String SCHEME_HTTPS = "https:";
+		private static final String SCHEME_EMAIL = "mailto:";
+
+		// Offset from the beginning of the EditText
+		private int mOffset;
+
+		// private static final Map<String, Integer> sSchemaActionResMap = new
+		// HashMap<String, Integer>();
+		// static {
+		// sSchemaActionResMap.put(SCHEME_TEL, R.string.tel);
+		// sSchemaActionResMap.put(SCHEME_HTTP, R.string.web);
+		// sSchemaActionResMap.put(SCHEME_HTTPS, R.string.web);
+		// sSchemaActionResMap.put(SCHEME_EMAIL, R.string.email);
+		// }
+		// we need this constructor for LayoutInflater
+		public LinedEditText(Context context, AttributeSet attrs) {
+			super(context, attrs);
+			setWillNotDraw(false);// 设置view是否更改，如果用自定义的view，重写ondraw()应该将调用此方法设置为false，这样程序会调用自定义的布局。
+			setLinkTextColor(0xFF0069FF);// 文字链接的颜色.
+			setLinksClickable(false);// 设置链接是否点击连接，即使设置了autoLink。
+			mRect = new Rect();
+			mPaint = new Paint();
+			PathEffect effects = new DashPathEffect(new float[] { 1, 2, 4, 8 },
+					1);
+			mPaint.setPathEffect(effects);
+			mPaint.setStyle(Paint.Style.STROKE);
+			mPaint.setStrokeWidth(1);
+			mPaint.setColor(Color.parseColor("#dbdbdb"));
+
+		}
+
+		@Override
+		protected void onDraw(Canvas canvas) {
+			int count = getLineCount();
+			Rect r = mRect;
+			Paint paint = mPaint;
+
+			for (int i = 0; i < count; i++) {
+				int baseline = getLineBounds(i, r);
+				Log.d(this, "LinedEditText onDraw baseline :" + baseline);
+				// canvas.drawLine(0, baseline + 5, r.right, baseline + 5,
+				// paint);
+			}
+			super.onDraw(canvas);
+		}
+	}
 }
---
+++ src/kg/gtss/note/NoteContentInputView.java
@@ -0,0 +1,14 @@
+package kg.gtss.note;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+public class NoteContentInputView extends EditText {
+
+	public NoteContentInputView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+}
